f1238bd2e9cbdffae16f9ba0d2cbea18ae248ab3
Chris Fung <aergonaut@gmail.com>
2019-07-17 10:34:59 -0700

    skeleton of apollo

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from "./greeter";
+import App from "./greeter";
 
-ReactDOM.render(<Hello name="world" />, document.getElementById("mount"));
+ReactDOM.render(<App />, document.getElementById("mount"));
--- src/js/greeter.tsx
+++ src/js/greeter.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-export default function Hello({ name }: { name: string }) {
+const client = new ApolloClient();
+
+function Hello({ name }: { name: string }) {
   return <h1>Hello {name}!!</h1>;
+}
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Hello name="Apollo" />
+    </ApolloProvider>
+  );
 }
