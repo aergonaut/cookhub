dc4e02aa7d6e1105237dcbb2bd399934eba4624f
Chris Fung <aergonaut@gmail.com>
2019-07-18 10:15:27 -0700

    add evergreen ui

--- src/js/greeter.tsx
+++ src/js/greeter.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { Pane, Heading } from "evergreen-ui";
 
 const client = new ApolloClient();
 
 function Hello({ name }: { name: string }) {
-  return <h1>Hello {name}!!</h1>;
+  return <Heading>Hello {name}!!</Heading>;
 }
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <Hello name="Apollo" />
+      <Pane width={1024} margin="auto">
+        <Hello name="Apollo" />
+      </Pane>
     </ApolloProvider>
   );
 }
